c847ab0e77bbf6a8d1f6f1e840bf75ee2dc6a07c
ZuhairQureshi <80606586+ZuhairQureshi@users.noreply.github.com>
2024-07-07 01:34:35 -0400

    Update main.py
    Revising documentation

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from fpdf import FPDF
 import yagmail
 import os 
 
-url = 'https://www.torontopubliclibrary.ca/search.jsp?N=37867+37744+37848&No=10&Ns=p_pub_date_sort&Nso=0'
 
+url = 'https://www.torontopubliclibrary.ca/search.jsp?N=37867+37744+37848&No=10&Ns=p_pub_date_sort&Nso=0'  # TPL 'Adult' Programs starting page
 library_dict = {}
 
 def get_driver():
+  ''' This function configures webdriver settings '''
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
   options.add_argument("start-maximized")
@@ -27,50 +28,60 @@ def get_driver():
   return driver
 
 
-def main():
+def main():  
   global url 
   global library_dict
   driver = get_driver()
 
+  # Counter variables (for output tracing - to ensure all listings are accounted for)
   page_count = 1
   event_count = 1
 
+  # Main scraping loop
   while True:
 
-    html_text = requests.get(url).text.replace("<br>", "")
+    # Get HTML and create Soup object
+    html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'html.parser')
-  
-    catalogue = soup.find_all('div', class_="row collapse")[2::2]
-    print(page_count)
-    for catalogue_item in catalogue:
-      event_name = catalogue_item.find('div', class_='title align-top').a.text
 
+    catalogue = soup.find_all('div', class_="row collapse")[2::2]  # get the element encapsulating all program listings
+
+    # Loop through all program listings on the page one-by-one
+    for catalogue_item in catalogue:
+
+      event_name = catalogue_item.find('div', class_='title align-top').a.text  # extract the title of the event
+
+      # Extract the library branch, either embedded within the link, or 'Online' otherwise
       try:
-        location = catalogue_item.find('div', class_="date-location").a.text
+        location = catalogue_item.find('div', class_="date-location").a.text   
 
       except:
         location = "Online"
-        
-      descrip = catalogue_item.find('div', class_='description').p.text.strip()
 
-      start_date = catalogue_item.find('span', class_='start-date').text
-      start_time = catalogue_item.find('div', class_='start-time') 
-      end_time = catalogue_item.find('div', class_='end-time')
-      print(start_date)
-      if start_date == 'on recurring dates listed below':
+      descrip = catalogue_item.find('div', class_='description').p.text.strip()  # get the truncated description
+
+      
+      start_date = catalogue_item.find('span', class_='start-date').text   # get the date 
+
+       # Advise user to check website if the date is non-specific
+      if start_date == 'on recurring dates listed below': 
         start_date = 'On recurring dates (please view link)'
-        
+
+      # Output tracing to ensure scraping matches webdriver activity
+      print(start_date)
       print(f"Event: {event_name} ({event_count})")
       print(f"Location: {location}")
       print(f"Description: {descrip}")
       
       event_count += 1
-      
+
+      # Redirect the webdriver to the next program listing and trace url
       sub_url = "https://www.torontopubliclibrary.ca" + catalogue_item.a['href'] + "\n"
       driver.get(sub_url)
       print(driver.current_url)
       print("\n")
 
+      # Order listings by library location
       if location not in library_dict:
         library_dict[location] = {
           'Event':[],
@@ -84,8 +95,10 @@ def main():
       library_dict[location]['Link'].append(driver.current_url)
       library_dict[location]['Date'].append(start_date)
 
+    # Return to listings page, so that 'Next' button can be located
     driver.get(url)
-  
+
+    # Check for the existence of the 'Next' button (i.e. if there is another page)
     if soup.find('li', class_='pagination-next') is not None:
       route = soup.find('li', class_='pagination-next').a['href']
       url = "https://www.torontopubliclibrary.ca/" + route 
@@ -93,54 +106,60 @@ def main():
       page_count += 1
   
     else:
-      break
+      break  # no 'Next' page indicates the final page has been scraped, so break the main loop
 
-  library_dict = dict(sorted(library_dict.items()))
+  library_dict = dict(sorted(library_dict.items()))  # alphabetically arrange the dictionary by location branch
 
-  generate_pdf()
+  generate_pdf()  # when the main loop, begin preparing the PDF 
 
 
 def generate_pdf():
+  '''This function creates the PDF using webscraped library program data according to branch location'''
   pdf = FPDF(orientation='P', unit='pt', format='A4')
   pdf.add_page()
 
+  # Create title of PDF and center it 
   pdf.set_font(family='Times', style='B', size=24)
   pdf.cell(w=0, h=50, txt="Library Programs This Week", align='C', ln=1)
-  
+
+  # Go through each listing by branch 
   for location in library_dict:
-    
+
+    # Display the location name 
     pdf.set_font(family='Times', style='B', size=16)
     pdf.cell(w=0, h=30, txt=location, ln=1)
-    
+
+    # Loop through every single listing associated with the library branch
     for i in range(len(library_dict[location]['Event'])):
 
+      # Extract all listing information 
       event = library_dict[location]['Event'][i]
       descrip = library_dict[location]['Description'][i]
       link = library_dict[location]['Link'][i]
       date = library_dict[location]['Date'][i]
       
-      # Event
+      # Display event information
       pdf.set_font(family='Times', style='B', size=14)
       pdf.cell(w=0, h=30, txt='Event', ln=1)
       
       pdf.set_font(family='Times', size=12)
       pdf.multi_cell(w=0, h=15, txt=event)
 
-      #Date
+      # Display date information
       pdf.set_font(family='Times', style='B', size=14)
       pdf.cell(w=0, h=30, txt='Date', ln=1)
 
       pdf.set_font(family='Times', size=12)
       pdf.multi_cell(w=0, h=15, txt=date)
       
-      # Description
+      # Display description
       pdf.set_font(family='Times', style='B', size=14)
       pdf.cell(w=0, h=30, txt='Description', ln=1)
 
       pdf.set_font(family='Times', size=12)
       pdf.multi_cell(w=0, h=15, txt=descrip)
 
-      # Link
+      # Display link, prompt viewer to click
       pdf.set_font(family='Times', style='B', size=14)
       pdf.cell(w=0, h=30, txt='Link:', ln=1)
 
@@ -150,21 +169,27 @@ def generate_pdf():
       pdf.set_text_color(0, 0, 0)
       
       pdf.cell(w=0, h=20, ln=1)
-    
-  pdf.output('Upcoming Library Events.pdf')
-  send_email()
+
+  pdf.output('Upcoming Library Events.pdf')  # output the PDF File
+  
+  send_email()  # prepare the email
 
 
 def send_email():
-  sender = 'zqu71658@gmail.com'
-  receiver ='selclovelena@gmail.com'
+  '''This function emails the end-user with the condensed PDF catalogue'''
+
+  # Create credentials
+  sender = 'SENDEREMAIL@gmail.com'
+  receiver ='RECEIVEREMAIL@gmail.com'
   password = os.getenv('PASSWORD')
   subject = "Toronto Public Libraries' Upcoming Programs"
 
+  # Put brief explainer, attach PDF, and email
   contents = ['''This PDF contains the 'Adult' listings for Toronto Public Library programs taking place in the next 7 days.''', 'Upcoming Library Events.pdf']
 
   yag = yagmail.SMTP(user=sender, password=password)
   yag.send(to=receiver, subject=subject, contents=contents)
   print("Mail sent.")
 
-send_email()
+
+main()
